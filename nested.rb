6480f091d6a751c3d3be1a81bc08b9f4c82b5902
lyle dickie <lyle.e.dickie@gmail.com>
2019-08-14 15:55:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -133,3 +133,23 @@ def adding_to_dennis
      }
 
 end
+
+
+
+
+
+programmer_hash = 
+ 		{
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+     }
