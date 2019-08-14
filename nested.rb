f756721e515012d2a678a1f2d8c2e3358b7e907a
lyle dickie <lyle.e.dickie@gmail.com>
2019-08-14 16:04:03 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -133,7 +133,7 @@ def adding_to_dennis
         }
      }
   
-  programmer_hash[:dennis_ritchie]
+  programmer_hash[:dennis_ritchie][:languages].push("Assembly")
   
   return programmer_hash
 end
